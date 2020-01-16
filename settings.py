64f17288355c968c81cee8c2db345c2aafd59da9
Abhijeet Sonawane <40629080+abhijeet-001@users.noreply.github.com>
2020-01-16 09:53:41 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 # Secret Key
-SECRET_KEY = "u0fo6l#^8%*80+)hv@^@ipry@rs8p59&7+zra+_*qji47_a64z"
+SECRET_KEY = "YOUR_SECRET_KEY"
 
 # Defines App's mode 
 # True or False
 DEBUG = False
 
 # Database Credentials
-DB_USER = "ieklqrecvqerkh"
-DB_PASSWORD = "196700c968985faf547b44ba0dc980d52e3007afac46133435fb8c6fbaf1a139"
-HOST = "ec2-174-129-230-117.compute-1.amazonaws.com"
-DB_NAME = "d4u24ev6fnq04p"
+DB_USER = "YOUR_DB_USER"
+DB_PASSWORD = "YOUR_DB_PASSWORD"
+HOST = "YOUR_DB_HOST"
+DB_NAME = "YOUR_DB_NAME"
 
 # SQLAlchemy Database URL
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://"+DB_USER+":"+DB_PASSWORD+"@"+HOST+"/"+DB_NAME
@@ -18,7 +18,7 @@ SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://"+DB_USER+":"+DB_PASSWORD+"@"+H
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Google Map API Key
-GOOGLEMAPS_KEY = 'AIzaSyCf9kPT4esIwoxc4hADTJJzW29QPOrdGbs'
+GOOGLEMAPS_KEY = 'YOUR_GOOGLEMAPS_KEY'
 
 # Ipinfo Token
-IPINFO_TOKEN = 'ad73729f464b23'
+IPINFO_TOKEN = 'YOUR_IPINFO_TOKEN'
